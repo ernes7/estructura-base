6263153e693b8584f081805a0f872bcc403140f7
Junior Estevez <rjr.eh1996@gmail.com>
2023-01-24 18:10:59 -0600

    Se crea el front

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css';
+// BOOTSTRAP 5
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
